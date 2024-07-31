36f3bd139b3b3319a939de3cf88c67feb6b1b672
Luís Fernando Almeida <luisfernalme@gmail.com>
2024-07-30 23:08:49 -0300

    QOL changes in the menu

--- main.py
+++ main.py
@@ -12,10 +12,30 @@
 # Pedro Augusto Simões da Cruz - 8116 - T1
 # -----------------------------------------------------------------
 
+from automaton import Automaton
 from interface import input_automaton, print_automaton
 from misc import check_equivalence, generate_automaton_image, generate_txt_report, simulate_word
 
 def main():
+    """
+    Example of how to define an automaton
+    states = ['q0', 'q1', 'q2', 'q3', 'q4']
+    alphabet = ['a', 'b']
+    transitions = {
+        'q0': {'a': ['q1']},
+        'q1': {'a': ['q3'], 'b': ['q2']},
+        'q2': {'a': ['q4'], 'b': ['q2']},
+        'q3': {'a': ['q3'], 'b': ['q2']},
+        'q4': {'a': ['q3'], 'b': ['q2']}
+    }
+
+    initial_state = 'q0'
+    final_states = ['q3', 'q4']
+    is_dfa = True
+
+    definido = Automaton(states, alphabet, transitions, initial_state, final_states, is_dfa)
+    """
+    
     automaton_instance = None
     converted_automaton = None
     minimized_automaton = None
@@ -23,69 +43,52 @@ def main():
     while True:
         print("\nMenu:\n")
         print("1. Insert automaton")
-        print("2. View automaton (print and create image)")
-        print("3. Convert automaton")
-        print("4. View converted automaton (print and create image)")
-        print("5. Minimize DFA")
-        print("6. View minimized DFA (print and create image)")
-        print("7. Simulate word acceptance in the inserted automaton")
-        print("8. Simulate word acceptance in the converted automaton")
-        print("9. Check equivalence between automata")
-        print("10. Generate .txt file with the automata")
+        print("2. Convert automaton to DFA")
+        print("3. Minimize DFA")
+        print("4. Simulate word acceptance (Inserted and converted automaton)")
+        print("5. Check equivalence between automata (Inserted and converted automaton)")
+        print("6. Generate .txt file")
         print("\n0. Exit")
         option = input("Choose an option: ")
 
         if option == "1":
             automaton_instance = input_automaton()
-        elif option == "2":
             if automaton_instance:
                 print_automaton(automaton_instance)
-                generate_automaton_image(automaton_instance, image_name="inserted_automaton", image_format="png")
-            else:
-                print("Automaton not inserted.")
-        elif option == "3":
+                image_name = "inserted_automaton.png"
+                generate_automaton_image(automaton_instance, image_name=image_name, image_format="png")
+        elif option == "2":
             if automaton_instance:
                 converted_automaton = automaton_instance.convert_to_dfa()
                 print("Automaton converted successfully.")
+                if converted_automaton:
+                    print_automaton(converted_automaton)
+                    image_name = "converted_automaton.png"
+                    generate_automaton_image(converted_automaton, image_name=image_name, image_format="png")
             else:
                 print("Automaton not inserted.")
-        elif option == "4":
-            if converted_automaton:
-                print_automaton(converted_automaton)
-                generate_automaton_image(converted_automaton, image_name="converted_automaton", image_format="png")
-            else:
-                print("Converted automaton not available.")
-        elif option == "5":
+        elif option == "3":
             if converted_automaton:
                 minimized_automaton = converted_automaton.minimize_dfa()
                 print("DFA minimized successfully.")
+                if minimized_automaton:
+                    print_automaton(minimized_automaton)
+                    image_name = "minimized_automaton.png"
+                    generate_automaton_image(minimized_automaton, image_name=image_name, image_format="png")
             else:
                 print("Converted automaton not available.")
-        elif option == "6":
-            if minimized_automaton:
-                print_automaton(minimized_automaton)
-                generate_automaton_image(minimized_automaton, image_name="minimized_automaton", image_format="png")
-            else:
-                print("Minimized DFA not available.")
-        elif option == "7":
-            if automaton_instance:
+        elif option == "4":
+            if automaton_instance or converted_automaton:
                 word = input("Enter the word to be simulated: ")
-                if simulate_word(automaton_instance, word):
-                    print("Word accepted by the inserted automaton.")
-                else:
-                    print("Word not accepted by the inserted automaton.")
+                if automaton_instance:
+                    result_instance = simulate_word(automaton_instance, word)
+                    print(f"Word acceptance by the inserted automaton: {'Accepted' if result_instance else 'Not accepted'}")
+                if converted_automaton:
+                    result_converted = simulate_word(converted_automaton, word)
+                    print(f"Word acceptance by the converted automaton: {'Accepted' if result_converted else 'Not accepted'}")
             else:
-                print("Automaton not inserted.")
-        elif option == "8":
-            if converted_automaton:
-                word = input("Enter the word to be simulated: ")
-                if simulate_word(converted_automaton, word):
-                    print("Word accepted by the converted automaton.")
-                else:
-                    print("Word not accepted by the converted automaton.")
-            else:
-                print("Converted automaton not available.")
-        elif option == "9":
+                print("Automaton not available.")
+        elif option == "5":
             if automaton_instance and converted_automaton:
                 if check_equivalence(automaton_instance, converted_automaton):
                     print("The automata are equivalent.")
@@ -93,7 +96,7 @@ def main():
                     print("The automata are not equivalent.")
             else:
                 print("Inserted or converted automaton not available.")
-        elif option == "10":
+        elif option == "6":
             if automaton_instance and converted_automaton and minimized_automaton:
                 generate_txt_report(automaton_instance, converted_automaton, minimized_automaton)
                 print("Report generated successfully.")
@@ -105,6 +108,6 @@ def main():
         else:
             print("Invalid option. Try again.")
 
-
 if __name__ == "__main__":
     main()
+
